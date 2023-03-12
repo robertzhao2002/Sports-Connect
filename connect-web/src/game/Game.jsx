ff0c3d5b8efa37f7f11d2dd41258a0bf2bd99d1a
Robert Zhao <rzhao1234@gmail.com>
2023-03-12 18:13:45 -0400

    Make game more compact

--- connect-web/src/game/Game.jsx
+++ connect-web/src/game/Game.jsx
@@ -60,7 +60,7 @@ function gameCopy(g) {
 export function PastGuesses() {
     return (<div align="center">
         <h1>
-            Past Guesses {
+            Past Guesses{
                 (pastGuesses().length > 0 ? `: ${numCorrect()}/${pastGuesses().length} (${(numCorrect() / pastGuesses().length * 100).toFixed(2)}%)` : '')
             }
         </h1>
@@ -68,21 +68,24 @@ export function PastGuesses() {
             <tbody>
                 <For each={pastGuesses()}>{item =>
                     <tr>
-                        <td><img height="100" width="100" src={item.imageUrl} /></td>
                         <td>
-                            <p>{item.name}</p>
-                            <p>{item.years.start}-{item.years.end}</p>
-                            <img src={`/pictures/${item.league}.png`} height="25" width="25" />
+                            <img height="75" width="75" src={item.imageUrl} />
+                            <br />
+                            <span>{item.name}</span>
+                            <br />
+                            <span>{item.years.start}-{item.years.end}</span>
+                            <br />
+                            <img src={`/pictures/${item.league}.png`} height="20" width="20" />
                         </td>
                         <td>
                             <For each={Object.entries(item.teams)}>{ty =>
-                                <div class="pastGuessesCol">
-                                    <img src={`/team-logos/${item.league}/${ty[0]}.png`} height="25" width="25" />
+                                <div class="teamYearEntry">
+                                    <img src={`/team-logos/${item.league}/${ty[0]}.png`} height="20" width="20" />
                                     <For each={ty[1]}>{yr =>
                                         <Show
                                             when={yr.end - yr.start > 0}
-                                            fallback={<span>({yr.start})</span>}>
-                                            <span>({yr.start}-{yr.end})</span>
+                                            fallback={` (${yr.start})\n`}>
+                                            {` (${yr.start}-${yr.end})\n`}
                                         </Show>
                                     }</For>
                                 </div>
@@ -91,13 +94,13 @@ export function PastGuesses() {
                         </td>
                         <Show
                             when={item.correct == true}
-                            fallback={<td><img src="/pictures/wrong.png" width="50px" height="50px" /></td>}>
+                            fallback={<td><img src="/pictures/wrong.png" width="40px" height="40px" /></td>}>
                             <td>
-                                <div class="pastGuessesCol">
-                                    <img src="/pictures/correct.png" width="50px" height="50px" />
-                                    <img src={`/team-logos/${item.league}/${item.correctTeams[0]}.png`} height="25" width="25" />
-                                    <span>⇔</span>
-                                    <img src={`/team-logos/${item.league}/${item.correctTeams[1]}.png`} height="25" width="25" />
+                                <img src="/pictures/correct.png" width="40px" height="40px" />
+                                <div class="teamYearEntry">
+                                    <img src={`/team-logos/${item.league}/${item.correctTeams[0]}.png`} height="20" width="20" />
+                                    <span> ⇔ </span>
+                                    <img src={`/team-logos/${item.league}/${item.correctTeams[1]}.png`} height="20" width="20" />
                                 </div>
                             </td>
                         </Show>
